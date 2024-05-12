2f6c13d73cf4c09ec62fcdb569ddfa3b1d4f6fc3
Rita A <35456@ufp.edu.pt>
2024-05-12 18:09:36 +0100

    updates

--- src/edu/ufp/inf/sd/rmi/project/client/Client.java
+++ src/edu/ufp/inf/sd/rmi/project/client/Client.java
@@ -1,15 +1,18 @@
 package edu.ufp.inf.sd.rmi.project.client;
 
-import edu.ufp.inf.sd.rmi.project.server.Const;
-import edu.ufp.inf.sd.rmi.project.server.Coordinate;
-import edu.ufp.inf.sd.rmi.project.server.Game;
-import edu.ufp.inf.sd.rmi.project.server.Sprite;
+import edu.ufp.inf.sd.rmi.project.server.*;
+import edu.ufp.inf.sd.rmi.util.rmisetup.SetupContextRMI;
 
 import java.io.IOException;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
@@ -18,7 +21,10 @@ public class Client {
    private Socket socket = null;
    static PrintStream out = null;
    static Scanner in = null;
+
    public static int id;
+   private GameFactoryRI gameFactory;
+   private SetupContextRMI contextRMI;
 
    final static int rateStatusUpdate = 115;
    public static Coordinate[][] map = new Coordinate[Const.LIN][Const.COL];
@@ -65,8 +71,56 @@ public class Client {
    }
    
    public static void main(String[] args) {
-      new Client("127.0.0.1", 8383);
-      new Window();
+      //new Client("127.0.0.1", 8383);
+      //new Window();
+      if (args != null && args.length < 2) {
+         System.err.println("usage: java [options] edu.ufp.sd.inf.rmi._04_diglib.client.DigLibClient <rmi_registry_ip> <rmi_registry_port> <service_name>");
+         System.exit(-1);
+      } else {
+         //1. ============ Setup client RMI context ============
+         Client clt = new Client(args);
+         //2. ============ Lookup service ============
+         clt.lookupService();
+         //3. ============ Play with service ============
+         clt.playService();
+      }
+   }
+
+   public Client(String args[]) {
+      try {
+         //List ans set args
+         SetupContextRMI.printArgs(this.getClass().getName(), args);
+         String registryIP = args[0];
+         String registryPort = args[1];
+         String serviceName = args[2];
+         //Create a context for RMI setup
+         contextRMI = new SetupContextRMI(this.getClass(), registryIP, registryPort, new String[]{serviceName});
+      } catch (RemoteException e) {
+         Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, e);
+      }
+   }
+
+   private void lookupService() {
+
+      //Get proxy MAIL_TO_ADDR rmiregistry
+      Registry registry = contextRMI.getRegistry();
+      //Lookup service on rmiregistry and wait for calls
+      if (registry != null) {
+         //Get service url (including servicename)
+         //String serviceUrl = contextRMI.getServicesUrl(0);
+         System.out.println("Registry is null");
+      }
+      String serviceUrl = contextRMI.getServicesUrl(0);
+      try {
+         gameFactory = (GameFactoryRI) registry.lookup(serviceUrl);
+      } catch (RemoteException | NotBoundException ex) {
+         ex.printStackTrace();
+      }
+   }
+
+   private void playService() {
+
+      //TODO ARRANJAR DO OBSERVER
    }
 }
 
--- src/edu/ufp/inf/sd/rmi/project/server/Server.java
+++ src/edu/ufp/inf/sd/rmi/project/server/Server.java
@@ -16,6 +16,7 @@ import java.io.UnsupportedEncodingException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
 import java.util.Properties;
 import java.util.function.BiConsumer;
 import java.util.logging.Level;
@@ -24,6 +25,8 @@ import java.util.logging.Logger;
 public class Server {
    public static PlayerData[] player = new PlayerData[Const.QTY_PLAYERS];
    public static Coordinate[][] map = new Coordinate[Const.LIN][Const.COL];
+
+   private GameFactoryRI gameFactory;
 
    /**
     * Context for running a RMI Servant on a SMTP_HOST_ADDR
@@ -122,7 +125,17 @@ public class Server {
 
    public static void main(String[] args) {
 
-      new Server(8383);
+      //new Server(8383);
+      if (args != null && args.length < 3) {
+         System.err.println("usage: java [options] edu.ufp.sd._04_diglib.server.DigLibServer <rmi_registry_ip> <rmi_registry_port> <service_name>");
+         System.exit(-1);
+      } else {
+         assert args != null;
+         //1. ============ Create Servant ============
+         Server srv = new Server(args);
+         //2. ============ Rebind servant on rmiregistry ============
+         srv.rebindService();
+      }
    }
 
    public static String giveToken(String username) {
@@ -158,6 +171,67 @@ public class Server {
          return true;
       }
       return false;
+   }
+
+   /**
+    *
+    * @param args
+    */
+   public Server(String args[]) {
+      try {
+         //============ List and Set args ============
+         SetupContextRMI.printArgs(this.getClass().getName(), args);
+         String registryIP = args[0];
+         String registryPort = args[1];
+         String serviceName = args[2];
+         //============ Create a context for RMI setup ============
+         contextRMI = new SetupContextRMI(this.getClass(), registryIP, registryPort, new String[]{serviceName});
+      } catch (RemoteException e) {
+         Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, e);
+      }
+   }
+
+   private void rebindService() {
+      try {
+         //Bind service on rmiregistry and wait for calls
+         if (this.contextRMI.getRegistry() != null) {
+            //============ Create Servant ============
+            gameFactory = new GameFactoryImpl();
+            //Get service url (including servicename)
+            String serviceUrl = contextRMI.getServicesUrl(0);
+            Logger.getLogger(this.getClass().getName()).log(Level.INFO, "going MAIL_TO_ADDR rebind service @ {0}", serviceUrl);
+            //============ Rebind servant ============
+            this.contextRMI.getRegistry().rebind(serviceUrl, gameFactory);
+            Logger.getLogger(this.getClass().getName()).log(Level.INFO, "service bound and running. :)");
+         } else {
+            Logger.getLogger(this.getClass().getName()).log(Level.INFO, "registry not bound (check IPs). :(");
+         }
+      } catch (RemoteException ex) {
+         Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+      }
+   }
+
+   @SuppressWarnings("unused")
+   private static void loadProperties() throws IOException {
+
+      Logger.getLogger(Thread.currentThread().getName()).log(Level.INFO, "going MAIL_TO_ADDR load props...");
+      // create and load default properties
+      Properties defaulltProps = new Properties();
+      FileInputStream in = new FileInputStream("defaultproperties.txt");
+      defaulltProps.load(in);
+      in.close();
+
+      BiConsumer<Object, Object> bc = (key, value) ->{
+         System.out.println(key.toString()+"="+value.toString());
+      };
+      defaulltProps.forEach(bc);
+
+      // create application properties with default
+      Properties properties = new Properties(defaulltProps);
+
+      FileOutputStream out = new FileOutputStream("defaultproperties2.txt");
+      properties.store(out, "---No Comment---");
+      out.close();
    }
 
 }
